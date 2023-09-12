4ac79edef091d93662fe864243dabe54d41deaef
MrKheirkhah <mrsoroushkheirkhah@gmail.com>
2023-09-12 23:32:47 +0330

    feat(*): Install dependencies and setup server, db, env variables

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose Connection ERROR: " + err.message);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected!");
+});
+
+const app = require("./app");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
